112db50a3593ede0bcd4b0f3bb5e69406d16b961
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-10-10 20:36:04 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Learn node.js');
+import express from 'express';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+const app = express();
+
+
+app.get('/',(req,res)=> {
+    res.status(200).json({message:"Okey is getting"});
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+})
